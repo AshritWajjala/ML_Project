dd69ef1504cfa30b2710097e981b4bfe317452a4
Ashrit Wajjala <ashritw2000@gmail.com>
2025-06-23 15:12:23 -0400

    Logging and Exception.

---
+++ src/exception.py
@@ -0,0 +1,19 @@
+import sys
+
+
+def error_message_detail(error, error_detail:sys):
+    _,_,exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message = ("Error occurced in Python Script."
+                     f"Name: [{file_name}], Line Number: {exc_tb.tb_lineno}, Error Message: {str(error)}")
+    
+    return error_message
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail:sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message,error_detail=error_detail)
+
+    def __str__(self):
+        return self.error_message
+
